f4dee895fdb1bcb692eac4688f2df918e4bc385a
owleek <owleek@mail.ru>
2020-08-27 17:07:28 +0300

    card-screen & list-screen-2

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,25 @@ $(document).ready(function(){
     event.preventDefault();
     $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top},500);
   });
+
+
+  $('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            dots:true
+        },
+        1000:{
+            items:1,
+            nav:true,
+            dots:true,
+            loop:false
+        }
+    }
+})
 });
 
