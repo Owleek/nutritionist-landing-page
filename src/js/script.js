43e865ee95c621e642843db67bd401dcef8bb0a4
owleek <owleek@mail.ru>
2020-08-28 00:24:36 +0300

    reviews

--- src/js/script.js
+++ src/js/script.js
@@ -5,23 +5,64 @@ $(document).ready(function(){
   });
 
 
-  $('.owl-carousel').owlCarousel({
+  $('#carousel-1').owlCarousel({
     loop: false,
     margin: 10,
+    autoHeight:true,
     responsiveClass:true,
     responsive:{
-        0:{
-            items:1,
-            nav:true,
-            dots:true
-        },
-        1000:{
-            items:1,
-            nav:true,
-            dots:true,
-            loop:false
-        }
+      0:{
+          items:1,
+          nav:true,
+          dots:true
+      },
+      1000:{
+          items:1,
+          nav:true,
+          dots:true,
+          loop:false
+      }
     }
-})
+  })
+
+  $('#carousel-2').owlCarousel({
+    loop: false,
+    margin: 10,
+    autoHeight:true,
+    responsiveClass:true,
+    responsive:{
+      0:{
+          items:1,
+          nav:true,
+          dots:true
+      },
+      1000:{
+          items:3,
+          nav:true,
+          dots:true,
+          loop:false
+      }
+    }
+  })
+
+  $('#carousel-3').owlCarousel({
+    loop: false,
+    margin: 10,
+    autoHeight:true,
+    responsiveClass:true,
+    responsive:{
+      0:{
+          items:1,
+          nav:true,
+          dots:true
+      },
+      1000:{
+          items:3,
+          nav:true,
+          dots:true,
+          loop:false
+      }
+    }
+  })
 });
 
